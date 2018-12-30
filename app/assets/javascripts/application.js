ed1579ce5a6ca0f67bc71eb329cf9af2cc892380
Jeff Katz <JeffreyEricKatz@gmail.com>
2018-12-29 21:07:49 -0500

    integrate fullpage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require now-ui-kit
+//= require fullpage
 //= require custom
 // can change bootstrap-sprockets to bootstrap for faster compilation
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   get 'about', to: 'pages#about'
+  get 'practice', to: 'pages#practice'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
