176cb16ab20d21437c7b519216f2c81d3467c7c6
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-03-05 15:03:11 -0500

    update navbar link urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'practice', to: 'pages#practice'
   get 'login', to: 'pages#login'
+  get 'course_details', to: 'pages#course_details'
+  resources :events
+  resources :applications
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
