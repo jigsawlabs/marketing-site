ae1ca503b7b734c2e27e9c39d9964b5169893ede
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-03-05 14:32:07 -0500

    add images, link paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :events
   get 'about', to: 'pages#about'
   get 'practice', to: 'pages#practice'
+  get 'login', to: 'pages#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
