44f09c6c439cfffce3909aaf80507f8c494799f5
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-03-05 21:06:27 -0500

    add code for google calendar link

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,4 +24,14 @@ class Event < ApplicationRecord
   def self.past
     Event.where(begin_time: 100.years.ago..0.days.from_now)
   end
+
+  def google_link
+      "https://www.google.com/calendar/render?action=TEMPLATE&text=#{self.title}&details=#{self.description}&location=#{self.location.full_address}&dates=#{self.google_calendar_time}"
+  end
+
+  def google_calendar_time
+    begin_time = Event.last.begin_time.utc.strftime('%Y%m%dT%H%M%SZ')
+    end_time = Event.last.end_time.utc.strftime('%Y%m%dT%H%M%SZ')
+    "#{begin_time}%2F#{end_time}"
+  end
 end
