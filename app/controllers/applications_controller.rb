4226ea2f87082c080d7d7df6afba251f0c8402b8
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-03-22 14:45:22 -0400

    have initial application form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,9 @@ class ApplicationsController < ApplicationController
   def new
     @application = Application.new
   end
+
+  def create
+    @application = Application.new(params.require(:application).permit(:first_name, :last_name, :email, :phone, :zipcode))
+    @application.save
+  end
 end
