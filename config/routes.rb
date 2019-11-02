e5d597520e8ed24636ac4473821f361d21c422d8
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-11-01 23:59:41 -0400

    change url, move course_signup to  path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'practice', to: 'pages#practice'
   get 'login', to: 'pages#login'
   get 'course_details', to: 'pages#course_details'
+
+  get "/course_signup" => redirect("http://learn.jigsawlabs.co/account/finish_auth?course_id=course-v1:masters+5+2019&enrollment_action=enroll&email_opt_in=false"), :as => :course_signup
   post 'update_application', to: 'applications#update_app', as: 'update_app'
   resources :events
   resources :applications
