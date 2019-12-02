2a636f98463bdcfd23a725ad5b2c5e315fa9cfb1
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-12-02 18:31:24 -0500

    fix link

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'login', to: 'pages#login'
   get 'course_details', to: 'pages#course_details'
 
-  get "/course_signup" => redirect("http://learn.jigsawlabs.co/account/finish_auth?course_id=course-v1:course-v1:masters+5+2019&enrollment_action=enroll&email_opt_in=false"), :as => :course_signup
+  get "/course_signup" => redirect("http://learn.jigsawlabs.co/account/finish_auth?course_id=course-v1:masters+5+2019&enrollment_action=enroll&email_opt_in=false"), :as => :course_signup
   post 'update_application', to: 'applications#update_app', as: 'update_app'
   resources :events
   get '/events_path' => redirect("https://www.meetup.com/Data-Workshops/events/"), :as => :events_path
