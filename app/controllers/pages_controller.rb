c3958f6c38f21b254f83b7b0c07a5627578d6304
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-01-05 15:12:04 -0500

    update curriculum lessons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,8 +18,8 @@ class PagesController < ApplicationController
   def free
     @intro_base_url = "https://colab.research.google.com/github/jigsawlabs-student/code-intro/blob/master/"
     @intro_links = ["0-starting-to-code", "1-visual-variables", "2-visual-lists", "3-visual-dicts", "4-review",
-                    "5-introduction-to-loops", "6-nested-data-structures", "7-a-review", "7-looping-through-live-data", "8-loops-to-lists",
-                "9-exploring-live-data", "10-saving-work-with-functions", "11-function-arguments", "12-code-to-codebase"
+                    "5-introduction-to-loops", "6-nested-data-structures", "7-make-it-easy", "8-looping-through-live-data", "9-loops-to-lists",
+                "10-exploring-live-data", "11-saving-work-with-functions", "12-function-arguments", "13-code-to-codebase"
     ]
     @intro_titles = ["Introduction", "Variables", "Lists", "Dictionaries", "Coding Tips",
                      "Loops", "Nested Data", "Make it Easy", "Loop Over Data", "Loops to Lists",
