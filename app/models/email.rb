5443437e18c62f49fc3874dd5517f032a415aede
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-09-03 12:22:36 -0400

    remove email send

--- app/models/email.rb
+++ app/models/email.rb
@@ -8,6 +8,7 @@ class Email < ApplicationRecord
                     "@api.mailgun.net/v3/#{DOMAIN_NAME}/messages",
                     :from => "jeff <jeff@#{DOMAIN_NAME}>",
                     :to => "#{self.to}, #{self.to}",
+                    :text => self.text,
                     :subject => self.welcome_subject,
                     :engine => 'handlebars',
                     :"v:first_name" => self.first_name,
@@ -15,11 +16,24 @@ class Email < ApplicationRecord
   end
 
   def welcome_subject
-    "Got your application #{self.first_name} :) Next steps."
+    "Got your application #{self.first_name}. Next steps."
   end
 
   def text
-    "Thanks for applying to"
+    """
+    Hi Jeff!
 
+So, as you might expect, the next step is for you to learn more about the program and for me to learn more about you. The best way to do this is with a friendly chat for about twenty-minutes.
+
+On the call, we'll talk about the course curriculum and how a typical class operates. We'll also talk about your interest in coding or data science and your desired outcome from the course.
+
+Schedule the phone call here.
+
+All the best,
+
+Jeff
+
+P.S. Don't worry! On the call, I will not be assessing any technical skills. If you're interesting in pursuing the data science career course, we'll talk through what you can expect in the technical interview for that course.
+"""
   end
 end
