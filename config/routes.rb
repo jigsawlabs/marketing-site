d0312a7af143640b7ee6f9264315bcb71e74dd4c
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-09-24 20:32:43 -0400

    update with hubspot link

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post 'update_application', to: 'applications#update_app', as: 'update_app'
   resources :events
   get '/events_path' => redirect("https://www.meetup.com/Data-Workshops/events/"), :as => :events_path
+  get '/applications/new' => redirect("https://meetings.hubspot.com/jeff525/-office-hours"), :as => :new_application_path
   resources :applications
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
